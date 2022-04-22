712b437d6fffea811b8b86a4fca5b2405b32ad36
Dan Cash <daniel.cash@egov.com>
2022-04-22 18:04:55 -0500

    added different endpoints

--- index.js
+++ index.js
@@ -12,15 +12,34 @@ http
   .createServer(async (req, res) => {
     const url = new URL(req.url, `http://${req.headers.host}`);
     console.log('Incoming request to:', url.pathname);
-    if (url.pathname === '/fibo') {
-      const n = randomInteger(20,50)
+    if (url.pathname === '/fibo1') {
+      // fast fibo
+      const n = randomInteger(1,20)
       console.log('Calculating fibonacci for', n);
       const result = await fibonacciWorker(n);
       res.writeHead(200);
       return res.end(`Result: ${result}\n`);
-    } else if (req.url == '/health') {
-      res.end("Healthy"); 
-      console.log("Request receieve at /health");
+    } else if (req.url == '/fibo2') {
+      // slower
+      const n = randomInteger(21,30)
+      console.log('Calculating fibonacci for', n);
+      const result = await fibonacciWorker(n);
+      res.writeHead(200);
+      return res.end(`Result: ${result}\n`);
+    } else if (req.url == '/fibo3') {
+      //slow
+      const n = randomInteger(31,40)
+      console.log('Calculating fibonacci for', n);
+      const result = await fibonacciWorker(n);
+      res.writeHead(200);
+      return res.end(`Result: ${result}\n`);
+    } else if (req.url == '/fibo4') {
+      //slowest
+      const n = randomInteger(41,50)
+      console.log('Calculating fibonacci for', n);
+      const result = await fibonacciWorker(n);
+      res.writeHead(200);
+      return res.end(`Result: ${result}\n`);
     } else {
       res.writeHead(200);
       return res.end('Hello World!');
