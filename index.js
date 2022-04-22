1e46f499d3a8cd4b7a05219f5ac1206fe3b29e6c
Dan Cash <daniel.cash@egov.com>
2022-04-22 17:57:09 -0500

    Changes

--- index.js
+++ index.js
@@ -3,21 +3,25 @@ const fibonacciWorker = require('./fibo-worker');
 
 const port = 8000;
 
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
 http
   .createServer(async (req, res) => {
     const url = new URL(req.url, `http://${req.headers.host}`);
     console.log('Incoming request to:', url.pathname);
-
     if (url.pathname === '/fibo') {
-      const n = Number(url.searchParams.get('n'));
+      const n = randomInteger(20,50)
       console.log('Calculating fibonacci for', n);
       const result = await fibonacciWorker(n);
       res.writeHead(200);
       return res.end(`Result: ${result}\n`);
     } else if (req.url == '/health') {
-    res.end("Healthy"); 
-	console.log("Request receieve at /health");
-	} else {
+      res.end("Healthy"); 
+      console.log("Request receieve at /health");
+    } else {
       res.writeHead(200);
       return res.end('Hello World!');
     }
